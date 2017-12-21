08591556ffccbe58f5af709b446e5c5529edb352
Prashant Singh <mprashantks@gmail.com>
2017-12-21 22:57:51 +0530

    Modified comments for better readability

--- sub-array-maximum-sum/sub-array-maximum-sum.cpp
+++ sub-array-maximum-sum/sub-array-maximum-sum.cpp
@@ -12,37 +12,55 @@ Entire array is traversed only once so giving a linear time complexity i.e., O(n
 #include <iostream>
 
 //Function to compute maximum number from passed arguments
-int max(int num1, int num2) {
+int max (int num1, int num2) {
   return (num1 >= num2 ? num1 : num2);
 }
 
 //Function to employ Kadane's algorithm
-int maxSum(int *arr, int size_array) {
+int maxSum (int *arr, int size_array) {
   int current_max, global_max;
+
+  // Intialise current_max and global_max with first element of array
   current_max = global_max = arr[0];
 
-  //Find if after including the array element pointed by pointer i the sum is greater than the global maximum sum
-  //If yes then update the global maximum
-  for(int i=1; i<size_array; i++) {
+  for (int i=1; i<size_array; i++) {
+
+    // current_max stores the current maximum sum i.e.,
+    // it stores the current maximum sum found so far in contiguous subarray
     current_max = max(arr[i], current_max+arr[i]);
-    if(current_max > global_max)
+
+    // global_max stores the sum of elements of a subarray which is greater than all other subarrays sum
+    // Condition to check if current_max is greater then global_max
+    // If yes then update global_max else skip this part
+    if (current_max > global_max)
       global_max = current_max;
+
   }
+
+  // Return the maximum sum of contiguous subarray
   return global_max;
 }
 
 
 //Main function
 int main () {
+
+  // Size of array to be entered
   int size_array;
+
+  // Console output and input
   std::cout << "\nEnter size of array: ";
 	std::cin >> size_array;
 
+  // Dynamically create an array of required size to avoid memory wastage
   int *arr = new int[size_array];
+
+  // Input array from console
   std::cout << "\nEnter array \n";
   for (int i=0; i<size_array; i++)
     std::cin >> arr[i];
 
+  // maxSum() function returns the maximum possible sum
   std::cout << "Maximum sum is: " << maxSum(arr, size_array) << std::endl;
 
   return 0;
